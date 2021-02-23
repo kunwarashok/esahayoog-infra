e0a31544f6ca5a08e956445a36227ed2f3052751
Ashok Kunwar <kunwarashok90@gmail.com>
2021-02-23 18:26:24 +0545

    installed auth package, added schemas

---
+++ database/migrations/2021_02_23_092559_create_account_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_details', function (Blueprint $table) {
+            $table->id();
+            $table->enum('accountType', ['esewa', 'imepay', 'khalti', 'bank' ,'fonepay', 'paypal']);
+            $table->string('accountName',70);
+            $table->string('accountNumber',16);
+            $table->foreign('bankId')->references('id')->on('banks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_details');
+    }
+}
